8137f6f8796c134c2e6c1b6ffc29377844a234da
Leandro Masello <leandro.masello@wolox.com.ar>
2019-01-04 14:31:19 -0300

    Remove redundant comments from migration

--- migrations/migrations/20190104135905-create-user.js
+++ migrations/migrations/20190104135905-create-user.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.createTable('Users', {
       id: {
         type: Sequelize.INTEGER,
@@ -46,13 +39,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return queryInterface.dropTable('Users');
   }
 };
