2db6843777dd671dfbf04920b9707a870febd10b
Leandro Masello <leandro.masello@wolox.com.ar>
2019-01-04 17:52:25 -0300

    Remove comments

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,5 @@
 const usersController = require('./controllers/usersController');
 
 exports.init = app => {
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
   app.post('/users', [], usersController.create);
 };
